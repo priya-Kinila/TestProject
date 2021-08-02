723ec0bedd9bd08c25a0d3f01aeaa1c0e7d6ecc4
Priya Kinila <priyakegaane@gmail.com>
2021-08-02 18:41:23 +0530

    Commiting Testclass to BranchX

---
+++ src/newpackage/TestClass.java
@@ -0,0 +1,29 @@
+package newpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+	
+	 public static void main(String[] args) {  
+	        
+		    // declaration and instantiation of objects/variables  
+		    System.setProperty("webdriver.chrome.driver", "C:\\Users\\PRIYA\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");  
+		    WebDriver driver=new ChromeDriver();  
+		      
+		// Launch website  
+		    driver.navigate().to("http://www.google.com/");  
+		          
+		    // Click on the search text box and send value  
+		    driver.findElement(By.name("q")).sendKeys("javatpoint tutorials");  
+		          
+		    // Click on the search button  
+		    driver.findElement(By.name("btnK")).click();  
+		    
+		    driver.close();
+		     
+		      
+		    }  
+
+}
